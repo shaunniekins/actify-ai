81b0e9e1ce443e7863d10f40e1e64334e0a880c8
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-03-06 00:50:45 +0800

    Add response from google ai

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,42 +1,68 @@
 "use client";
 
 import { useState } from "react";
+import { runChat } from "../api/gemini-ai";
+
+type Message = {
+  sender: "user" | "model";
+  text: string;
+};
 
 const DashboardComponent = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [processing, setProcessing] = useState<boolean>(false);
-  const [response, setResponse] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
 
-  const handleSubmit = (e: React.KeyboardEvent) => {
+  const handleSubmit = async (e: React.KeyboardEvent) => {
     if (e.key === "Enter") {
+      setPrompt("");
       setProcessing(true);
-      resetPrompt();
-      console.log(prompt);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { sender: "user", text: prompt },
+      ]);
+      const chatResponse = await runChat(prompt);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { sender: "model", text: chatResponse },
+      ]);
+      setProcessing(false);
+      //   console.log(chatResponse);
     }
   };
 
   const resetPrompt = () => {
-    setPrompt("");
+    setMessages([]);
   };
 
   return (
     <>
-      <div className="h-[100svh] w-screen py-8">
-        <div className="container mx-auto w-screen h-full">
-          <div className="flex flex-col justify-between h-full px-96">
+      <div className="h-[100svh] w-screen py-8 overflow-hidden">
+        <div className="md:container md:mx-auto w-screen h-full">
+          <div className="flex flex-col gap-3 justify-between h-full px-5 md:px-96">
             <button
-              className="px-5 py-2 bg-red-300 self-end rounded-lg"
-              onClick={() => resetPrompt}>
+              className="px-5 py-2 bg-red-600 self-end rounded-lg"
+              onClick={resetPrompt}>
               Restart
             </button>
             {/* <h1>hello</h1> */}
-            <div className="bg-red-600 h-full"></div>
-            <input
-              className="rounded-lg bg-gray-300 text-black w-full py-3 px-2"
+            <div className="py-5 h-full flex flex-col gap-5 overflow-y-auto">
+              {messages.map((message, index) => (
+                <p
+                  key={index}
+                  className={`${
+                    message.sender === "user"
+                      ? "text-right text-sky-600"
+                      : "text-left text-white"
+                  }`}>
+                  {message.text}
+                </p>
+              ))}
+            </div>
+            <textarea
+              className="resize-none overflow-hidden rounded-lg bg-gray-300 text-black w-full py-3 px-2"
+              rows={1}
               disabled={processing}
-              type="text"
-              name="promptInput"
-              id="promptInput"
               placeholder="Enter prompt here"
               value={prompt}
               onChange={(e) => setPrompt(e.target.value)}
